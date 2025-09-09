292c8ef94edc4e29cc99849ee329721af2b89a9f
leszek <lechgrzelak@gmail.com>
2025-09-09 13:08:08 +0200

    Update Randomized_Bates_model.m

--- Randomized_Bates_model.m
+++ Randomized_Bates_model.m
@@ -7,11 +7,16 @@ close all;clc;
 % L.A. Grzelak@uu.nl
 % 
 % @article{grzelakRAnD,
-%   title={On Randomization of Affine Diffusion Processes with Application to Pricing of Options on {VIX} and {S&P} 500},
-%   author={Grzelak, Lech A.},
-%   journal={arXiv:2208.12518},  
-%   year = {2022}
-% }
+%title = {On randomization of affine diffusion processes with application to pricing of options on VIX and S&P 500},
+%journal = {Applied Mathematics and Computation},
+%volume = {508},
+%pages = {129598},
+%year = {2026},
+%issn = {0096-3003},
+%doi = {https://doi.org/10.1016/j.amc.2025.129598},
+%url = {https://www.sciencedirect.com/science/article/pii/S0096300325003248},
+%author = {Lech A. Grzelak},
+%}
 
 K = linspace(60,140,25)/100;
 
@@ -233,4 +238,5 @@ function [chi_k,psi_k] = Chi_Psi(a,b,c,d,k)
     expr2 = k * pi / (b - a) .* sin(k * pi * ...
                         (d - a) / (b - a))   - k * pi / (b - a) .* sin(k... 
                         * pi * (c - a) / (b - a)) * exp(c);
+
     chi_k = chi_k .* (expr1 + expr2);
