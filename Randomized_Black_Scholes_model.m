e423fe706a069955e365af8df702eccef89df466
leszek <lechgrzelak@gmail.com>
2025-09-09 13:09:44 +0200

    Update Randomized_Black_Scholes_model.m

--- Randomized_Black_Scholes_model.m
+++ Randomized_Black_Scholes_model.m
@@ -6,12 +6,17 @@ close all;clc;
 % with Application to Pricing of Options on VIX and S&P 500' by Lech A. Grzelak, 
 % L.A. Grzelak@uu.nl
 % 
-% @article{grzelakRAnD,
-%   title={On Randomization of Affine Diffusion Processes with Application to Pricing of Options on {VIX} and {S&P} 500},
-%   author={Grzelak, Lech A.},
-%   journal={arXiv:2208.12518},  
-%   year = {2022}
-% }
+%@article{grzelakRAnD,
+%title = {On randomization of affine diffusion processes with application to pricing of options on VIX and S&P 500},
+%journal = {Applied Mathematics and Computation},
+%volume = {508},
+%pages = {129598},
+%year = {2026},
+%issn = {0096-3003},
+%doi = {https://doi.org/10.1016/j.amc.2025.129598},
+%url = {https://www.sciencedirect.com/science/article/pii/S0096300325003248},
+%author = {Lech A. Grzelak},
+%}
 
 r         = 0.0;
 S0        = 1;
@@ -197,4 +202,5 @@ function [chi_k,psi_k] = Chi_Psi(a,b,c,d,k)
     expr2 = k * pi / (b - a) .* sin(k * pi * ...
                         (d - a) / (b - a))   - k * pi / (b - a) .* sin(k... 
                         * pi * (c - a) / (b - a)) * exp(c);
+
     chi_k = chi_k .* (expr1 + expr2);
