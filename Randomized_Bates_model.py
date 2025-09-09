ec9520479e53f176830fdbf05df8fa19abbdbe2e
leszek <lechgrzelak@gmail.com>
2025-09-09 13:09:12 +0200

    Update Randomized_Bates_model.py

--- Randomized_Bates_model.py
+++ Randomized_Bates_model.py
@@ -7,10 +7,15 @@ with Application to Pricing of Options on VIX and S&P 500' by Lech A. Grzelak,
 L.A. Grzelak@uu.nl
 
 @article{grzelakRAnD,
-  title={On Randomization of Affine Diffusion Processes with Application to Pricing of Options on {VIX} and {S&P} 500},
-  author={Grzelak, Lech A.},
-  journal={arXiv:2208.12518},  
-  year = {2022}
+title = {On randomization of affine diffusion processes with application to pricing of options on VIX and S&P 500},
+journal = {Applied Mathematics and Computation},
+volume = {508},
+pages = {129598},
+year = {2026},
+issn = {0096-3003},
+doi = {https://doi.org/10.1016/j.amc.2025.129598},
+url = {https://www.sciencedirect.com/science/article/pii/S0096300325003248},
+author = {Lech A. Grzelak},
 }
 
 @author: LECH A. GRZELAK
@@ -304,5 +309,6 @@ def main():
         plt.plot(K,IVRAnD)
     
     plt.legend(label)
+
 
 main()
